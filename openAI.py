a78a1246bc002e94a70ad76469de33c9b5f01992
Livia Fares <lfares@amazon.com>
2024-10-06 06:46:05 -0300

    Added final descriptions and fixed OpenAI prompt

--- openAI.py
+++ openAI.py
@@ -24,67 +24,80 @@ def retrieve_modules(subjects):
     return modules
 
 def retrieve_enem_question(questionId):
-    questionsFile = open('./enem2023/NaturalScience.json')
+    questionsFile = open('./enem2023/Math.json')
     questionsJson = json.load(questionsFile)
     questionsFile.close()
 
-    return questionsJson[questionId]['text'], questionsJson[questionId]['statement'], questionsJson[questionId]['alternatives']
+    extraResourcesDescription = ""
+    if ('extra_resources' in questionsJson[questionId]):
+        extraResourcesDescription = questionsJson[questionId]['extra_resources']['description']
 
-def create_openAi_user_content(modules, questionText, questionStatement, alternatives):
+    return questionsJson[questionId]['text'], questionsJson[questionId]['statement'], questionsJson[questionId]['alternatives'], extraResourcesDescription
+
+def create_openAi_user_content(modules, questionText, questionStatement, alternatives, extraResourcesDescription):
     content = "<modules>"
     for module in modules:
         content = content + module + '; '
     
-    content = content + '</modules> \n <question_text>' + questionText + '</question_text> \n <question_statement>' + questionStatement + '</question_statement> \n <alternatives>'
+    content = content + '</modules> \n <question_text>' + questionText + '</question_text> \n'
+    if extraResourcesDescription:
+        content = content + '<extra_resources_description>' + extraResourcesDescription + '</extra_resources_description> \n'
+    content = content + '<question_statement>' + questionStatement + '</question_statement> \n <alternatives>'
+
     for alternativeId in alternatives.keys():
-        content = content + alternativeId + '.' + alternatives[alternativeId] + '; '
+        if isinstance(alternatives[alternativeId], dict):
+            content = content + alternativeId + '.' + alternatives[alternativeId]['description'] + '; '
+        else:
+            content = content + alternativeId + '.' + alternatives[alternativeId] + '; '
 
     content = content + '</alternatives>'
     return content
 
 def add_modules_to_json(questionId, modules):
-    with open('./todaTeoria/ModulesIdByEnemQuestion.json', 'r+') as modulesIdFile:
+    with open('./enem2023/definedModules/ModulesIdByMathQuestion.json', 'r+') as modulesIdFile:
         modulesIdJson = json.load(modulesIdFile)
 
         newQuestion = {questionId: []}
         for module in modules:
             moduleId = re.findall(r'\d+', module)[0]
             newQuestion[questionId].append(moduleId)
+        newQuestion[questionId] = list(set(newQuestion[questionId]))
 
         modulesIdJson.update(newQuestion)
         modulesIdFile.seek(0)
         json.dump(modulesIdJson, modulesIdFile)
     
 
-enemComponent = "naturalScience" # can be also retrieved from user
-questionId = "3" # can be also retrieved from user
-todaTeoriaSubjects = retrieve_subjects(enemComponent)
-todaTeoriaModules = retrieve_modules(todaTeoriaSubjects)
-questionText, questionStatement, alternatives = retrieve_enem_question(questionId)
-userContent = create_openAi_user_content(todaTeoriaModules, questionText, questionStatement, alternatives)
-print(userContent)
+enemComponent = "math" # can be also retrieved from user
+for i in range(1, 46):
+    questionId = str(i) # can be also retrieved from user
+    todaTeoriaSubjects = retrieve_subjects(enemComponent)
+    todaTeoriaModules = retrieve_modules(todaTeoriaSubjects)
+    questionText, questionStatement, alternatives, extraResourcesDescription = retrieve_enem_question(questionId)
+    userContent = create_openAi_user_content(todaTeoriaModules, questionText, questionStatement, alternatives, extraResourcesDescription)
+    print(userContent)
 
-client = OpenAI()
-miniModel = "gpt-4o-mini"
-systemContent = "You will be provided with a set of modules from topics of high school education in Brazil, and a multiple choice question from ENEM, with 5 alternatives to choose from (delimited with XML tags). Please recommend, at most, the top 3 modules that students should study to answer the question. \n The modules should be specifically related with the question's theme. \n The answer should contain only the choosen modules. \n The user statement will be done in portuguese"
+    client = OpenAI()
+    miniModel = "gpt-4o-mini"
+    systemContent = "You are tasked with assisting a student in preparing for the ENEM exam. \n You will be provided with a set of modules from topics of high school education in Brazil, and a multiple choice question from ENEM, brazilian test to enter universities. \n The question is related with {enemComponent} subject, and it might contain an extra resource description of an image or table to help answer it. The question contains 5 alternatives to choose from, with only one correct. \n Everything will be delimited by XML tags to help identify them.  \n Your task is to identify two modules from the list that are most relevant to helping the student answer the question accurately. \n The modules chosen must be specifically related with the question's main theme, and mastering those modules must enable a person to choose the correct answer without further knowledge. \n Work out the question first, then analyze the content of each module and, finally, choose two modules that would best prepare the student to answer the question. \n The user statement will be done in portuguese \n Provide the number of each relevant module in the format *number*, ensuring that the numbers are enclosed separately in different * symbols"
 
-completion = client.chat.completions.create(
-    model=miniModel,
-    messages=[
-        {
-            "role": "system", 
-            "content": systemContent
-        },
-        {
-           "role": "user",
-            "content": userContent
-        }
-    ]
-)
+    completion = client.chat.completions.create(
+        model=miniModel,
+        messages=[
+            {
+                "role": "system", 
+                "content": systemContent
+            },
+            {
+            "role": "user",
+                "content": userContent
+            }
+        ]
+    )
 
-print(completion.choices[0].message)
-foundModules = str(completion.choices[0].message).split('<modules>')[1].split('; ')
-print(foundModules)
-add_modules_to_json(questionId, foundModules)
+    print(completion.choices[0].message)
+    foundModules = re.findall(r'\*(\d+)\*', str(completion.choices[0].message))
+    print(foundModules)
+    add_modules_to_json(questionId, foundModules)
 
 
